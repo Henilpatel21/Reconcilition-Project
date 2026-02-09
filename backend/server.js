ec734a5f8c1460007c2691e9d24e131c8af9593b
Henil Patel <henilpatel1008@gmail.com>
2026-02-10 01:38:33 +0530

    Some important CORS changes

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,8 @@ if (!CLIENT_URL) {
     cors({
       origin: process.env.CLIENT_URL,
       credentials: true,
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+      allowedHeaders: ["Content-Type", "Authorization"],
     })
   );
 }
